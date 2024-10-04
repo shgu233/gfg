cefaf9788257f939de04afeb4af34ba13c6b446b
Shreya Gupta <108684104+shgu233@users.noreply.github.com>
2024-10-04 10:59:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/if-else (Decision Making)/ifelse-decision-making.py
@@ -0,0 +1,33 @@
+
+class Solution:
+    def compareNM(self, n : int, m : int) -> str:
+        # code here
+        
+        if n<m:
+            return "lesser"
+        elif n==m:
+            return"equal"
+        else:
+            return"greater"
+
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        m = int(input())
+        
+        obj = Solution()
+        res = obj.compareNM(n, m)
+        
+        print(res)
+        
+
+# } Driver Code Ends
